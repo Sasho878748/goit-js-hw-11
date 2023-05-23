be6dd0ff746d8a707967e5516ad58057c0140e89
Sasho878748 <kovalenko878748@gmail.com>
2023-05-23 06:39:08 +0300

    create fix1

--- src/js/SearchService.js
+++ src/js/SearchService.js
@@ -2,28 +2,31 @@ import axios from 'axios';
 
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '36471035-8dd0fed22317ca26f46d35a21';
+const searchParams = new URLSearchParams({
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+}).toString();
 
 export default class SearchService {
   constructor() {
     this.page = 1;
     this.q = '';
+    this.totalHitsPhoto = '';
+    this.hitsPhoto = '';
+    this.photoBeenDownload = 0;
   }
 
   async getNews() {
-    try {
-      const response = await axios.get(
-        `${URL}/?key=${API_KEY}&q=${this.q}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
-      );
-      const { hits, totalHits } = response.data;
-      this.incrementPage();
-      return {
-        hits,
-        totalHits,
-      };
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    const response = await axios.get(
+      `${URL}?key=${API_KEY}&q=${this.q}&per_page=40&page=${this.page}&${searchParams}`
+    );
+    const { hits, totalHits } = response.data;
+    this.incrementPage();
+    return {
+      hits,
+      totalHits,
+    };
   }
 
   resetPage() {
